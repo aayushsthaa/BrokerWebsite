130e06dfb9a09d64ff323fd0c9b1949c96962859
Aayush Shrestha <aayushsth77@gmail.com>
2026-03-29 19:53:09 +0545

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 var app = express();
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "https://urbaniva.onrender.com"
 }));
 
 app.use(logger('dev'));
@@ -22,7 +22,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
